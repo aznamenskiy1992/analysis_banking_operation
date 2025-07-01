2c9a3ec4e96c13ad78d6f0c910b4f197c4cab75c
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-01 22:00:53 +0300

    обработал кейс по возврату отфильтрованных операций по строке поиска. Для функции filter_transaction_by_search_str модуля services

--- src/services.py
+++ src/services.py
@@ -1,5 +1,13 @@
+import pandas as pd
+import json
 from src.data import get_data
 
 
-def filter_transaction_by_search_str(search_str: str):
-    pass
+def filter_transaction_by_search_str(search_str: str) -> json:
+    """Функция фильтрует DataFrame с операцими по строке"""
+    search_str_lower = search_str.lower()
+
+    operations: pd.DataFrame = get_data()
+    operations_to_list: list[dict] = operations.to_dict(orient='records')
+
+    return json.dumps([item for item in operations_to_list if search_str_lower in item['Категория'].lower() or search_str_lower in item['Описание'].lower()])
