033805112378139f8b691c52110f26ae8329ada6
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-01 20:37:38 +0300

    обработал кейс, когда файл не найден. Для функции get_data модуля data

--- src/data.py
+++ src/data.py
@@ -1,2 +1,9 @@
-def get_data():
-    pass
+import pandas as pd
+
+
+def get_data() -> pd.DataFrame:
+    """Функция возвращает DataFrame с банковскими операциями"""
+    try:
+        return pd.read_excel('data/operations.xlsx')
+    except FileNotFoundError:
+        raise FileNotFoundError('Файл не найден')
