1efa61add5f4f3f8458929815083fdcbfdf8a035
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-02 20:55:56 +0300

    исправил код в функции filter_transaction_by_search_str модуля services

--- src/services.py
+++ src/services.py
@@ -5,7 +5,7 @@ import pandas as pd
 from src.data import get_data
 
 
-def filter_transaction_by_search_str(search_str: str) -> str:
+def filter_transaction_by_search_str(operation: list[dict], search_str: str) -> str:
     """
     Фильтрует транзакции по строке поиска, проверяя совпадения в полях 'Категория' и 'Описание'.
 
@@ -13,6 +13,7 @@ def filter_transaction_by_search_str(search_str: str) -> str:
         search_str (str): Строка для поиска в транзакциях. Регистр не учитывается.
 
     Возвращает:
+        operation: список словарей с транзакциями
         str: JSON-строка с отфильтрованными транзакциями, где:
              - search_str найдена в поле 'Категория' (без учета регистра)
              - ИЛИ search_str найдена в поле 'Описание' (без учета регистра)
@@ -31,12 +32,6 @@ def filter_transaction_by_search_str(search_str: str) -> str:
     # Приведение строки поиска к нижнему регистру для регистронезависимого поиска
     search_str_lower = search_str.lower()
 
-    # Получение данных транзакций
-    operations: pd.DataFrame = get_data()
-
-    # Конвертация DataFrame в список словарей для удобной обработки
-    operations_to_list: list[dict] = operations.to_dict(orient="records")
-
     # Фильтрация и возврат результата в формате JSON:
     # 1. Итерируемся по всем транзакциям
     # 2. Проверяем наличие search_str в полях 'Категория' или 'Описание' (без учета регистра)
@@ -44,7 +39,7 @@ def filter_transaction_by_search_str(search_str: str) -> str:
     return json.dumps(
         [
             item
-            for item in operations_to_list
+            for item in operation
             if search_str_lower in item["Категория"].lower() or search_str_lower in item["Описание"].lower()
         ]
     )
--- tests/test_services.py
+++ tests/test_services.py
@@ -6,12 +6,9 @@ import pytest
 from src.services import filter_transaction_by_search_str
 
 
-@patch("src.services.get_data")
-def test_get_transaction_for_filter_transaction_by_search_str(mock_get_data, get_data_for_services):
+def test_get_transaction_for_filter_transaction_by_search_str(get_data_for_services):
     """Тестирует возврат отфильтрованных операций по строке поиска"""
-    mock_get_data.return_value = get_data_for_services
-
-    result = filter_transaction_by_search_str("МА")
+    result = filter_transaction_by_search_str(get_data_for_services, "МА")
 
     assert json.loads(result) == [
         {
@@ -68,31 +65,22 @@ def test_get_transaction_for_filter_transaction_by_search_str(mock_get_data, get
     ]
 
 
-@patch("src.services.get_data")
-def test_transactions_not_found_for_filter_transaction_by_search_str(mock_get_data, get_data_for_services):
+def test_transactions_not_found_for_filter_transaction_by_search_str(get_data_for_services):
     """Тестирует кейс, когда транзакции не найдены"""
-    mock_get_data.return_value = get_data_for_services
-
-    result = filter_transaction_by_search_str(".")
+    result = filter_transaction_by_search_str(get_data_for_services, ".")
 
     assert json.loads(result) == []
 
 
-@patch("src.services.get_data")
-def test_none_search_str_for_filter_transaction_by_search_str(mock_get_data, get_data_for_services):
+def test_none_search_str_for_filter_transaction_by_search_str(get_data_for_services):
     """Тестирует кейс, когда не передана строка поиска"""
-    mock_get_data.return_value = get_data_for_services
-
     with pytest.raises(ValueError) as exc_info:
-        filter_transaction_by_search_str(None)
+        filter_transaction_by_search_str(get_data_for_services, None)
     assert str(exc_info.value) == "Строка для поиска не передана"
 
 
-@patch("src.services.get_data")
-def test_search_is_not_str_for_filter_transaction_by_search_str(mock_get_data, get_data_for_services):
+def test_search_is_not_str_for_filter_transaction_by_search_str(get_data_for_services):
     """Тестирует кейс, когда cтрока передана не в типе str"""
-    mock_get_data.return_value = get_data_for_services
-
     with pytest.raises(TypeError) as exc_info:
-        filter_transaction_by_search_str(["МА"])
+        filter_transaction_by_search_str(get_data_for_services, ["МА"])
     assert str(exc_info.value) == "Строка передана не в типе str"
