26214c82c6d9972dac528d2b7a49ddc611fae66a
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-02 21:28:21 +0300

    создал тест кейса по возврату трат за 3 месяца по категории. Для функции get_expenses_for_3_months_by_category модуля reports

---
+++ src/reports.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+import pandas as pd
+import datetime
+
+
+def get_expenses_for_3_months_by_category(operation: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    pass
