6e3b5ef4d09e76872e4667335407ec521a89746e
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-02 21:33:56 +0300

    исправил тест кейса по возврату трат за 3 месяца по категории. Для функции get_expenses_for_3_months_by_category модуля reports

--- src/reports.py
+++ src/reports.py
@@ -4,5 +4,5 @@ import pandas as pd
 import datetime
 
 
-def get_expenses_for_3_months_by_category(operation: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
-    pass
+def get_expenses_for_3_months_by_category(operation: pd.DataFrame, category: str, date: Optional[str] = None) -> str:
+    """Функция возвращает траты по категории за последние 3 месяца"""
