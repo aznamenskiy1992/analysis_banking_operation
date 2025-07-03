a88739ca87e6c20909e641bfdd90d1cb42122045
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-07-03 19:36:27 +0300

    обработал кейс по возврату трат за 3 месяца по категориям. Для функции get_expenses_for_3_months_by_category модуля reports

--- src/reports.py
+++ src/reports.py
@@ -1,3 +1,4 @@
+import json
 from typing import Optional
 
 import pandas as pd
@@ -6,3 +7,33 @@ import datetime
 
 def get_expenses_for_3_months_by_category(operation: pd.DataFrame, category: str, date: Optional[str] = None) -> str:
     """Функция возвращает траты по категории за последние 3 месяца"""
+    if date is None:
+        date = datetime.datetime.now()
+
+    date_obj = datetime.datetime.strptime(date, '%Y-%m-%d').replace(hour=23, minute=59, second=59)
+    print(date_obj)
+
+    if not pd.api.types.is_datetime64_dtype(operation['Дата операции']):
+        operation['Дата операции'] = pd.to_datetime(operation['Дата операции'], dayfirst=True)
+        print(operation['Дата операции'].dtype)
+
+    normalize_category: str = category.strip().capitalize()
+    print(normalize_category)
+
+    start_date = date_obj - datetime.timedelta(days=90)
+    print(start_date)
+
+    filtered_operation = operation.loc[
+        (operation['Дата операции'].notnull()) &
+        (operation['Категория'].notnull()) &
+        (operation['Категория'] == normalize_category) &
+        (operation['Дата операции'] >= start_date) &
+        (operation['Дата операции'] <= date_obj)
+    ]
+    print(filtered_operation.head())
+
+    if len(filtered_operation) != 0:
+        grouped_operation = filtered_operation.groupby('Категория')['Сумма операции с округлением'].sum().reset_index()
+        return json.dumps(grouped_operation.to_dict(orient='records'))
+    else:
+        return json.dumps([])
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -7,19 +7,14 @@ import json
 from src.reports import get_expenses_for_3_months_by_category
 
 
-@patch('datetime.now')
-@pytest.mark.parametrize(
-    'date_',
-    [ '2020,12,31', None]
-)
-def test_get_expenses_for_get_expenses_for_3_months_by_category(mock_now_date, date_, get_data_for_reports):
+def test_get_expenses_for_get_expenses_for_3_months_by_category(get_data_for_reports):
     """Тестирует возврат трат за 3 месяца по категории"""
-    mock_now_date = datetime.datetime(2021, 12, 31, 16, 44, 00)
 
-    result = get_expenses_for_3_months_by_category(get_data_for_reports, 'Супермаркеты', date_)
+    result = get_expenses_for_3_months_by_category(get_data_for_reports, 'Супермаркеты', '2021-12-31')
 
     assert json.loads(result) == [
         {
-            "Супермаркеты": 321.78,
+            "Категория": "Супермаркеты",
+            "Сумма операции с округлением": 321.78,
         },
     ]
